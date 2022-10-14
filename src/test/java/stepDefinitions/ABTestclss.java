f1a941d82e77b241c5bd4a892bd97faf02337650
Manasavi Sharma <sharma.manasavi@gmail.com>
2022-10-14 15:50:55 +0530

    cucumber file1

--- src/test/java/stepDefinitions/ABTestclss.java
+++ src/test/java/stepDefinitions/ABTestclss.java
@@ -11,7 +11,7 @@ public class ABTestclss {
     WebDriver driver;
 
     @Given("user navigates to <Page Type> page")
-    public void user_navigates_to_page_type_page() {
+    public void navigates() {
         // Write code here that turns the phrase above into concrete actions
         System.setProperty("webdriver.chrome.driver", "src/lib/chromedriver.exe");
         driver= new ChromeDriver();
@@ -20,14 +20,14 @@ public class ABTestclss {
     }
 
     @When("user clicks on the <Add button>")
-    public void user_clicks_on_the_add_button() {
+    public void clicks_button() {
         // Write code here that turns the phrase above into concrete actions
         driver.findElement(By.xpath("//button[contains(text(),'Add Element')]")).click();
         throw new io.cucumber.java.PendingException();
     }
 
     @Then("user can view <Delete button>")
-    public void user_can_view_delete_button() {
+    public void delete_button() {
         // Write code here that turns the phrase above into concrete actions
         driver.quit();
         throw new io.cucumber.java.PendingException();
